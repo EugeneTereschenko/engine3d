48af338b2a43e3ee2593944325341163bf82f1af
Yevhen_Tereshchenko <Yevhen_Tereshchenko@epam.com>
2025-01-27 16:36:06 +0200

    Initial commit

---
+++ src/main/java/org/example/engine/DDDTutorial.java
@@ -0,0 +1,19 @@
+package org.example.engine;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DDDTutorial {
+    static Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    static JTextField TF;
+
+    public static void main(String[] args) {
+        JFrame F = new JFrame();
+        Screen ScreenObject = new Screen();
+        F.add(ScreenObject);
+        F.setUndecorated(true);
+        F.setSize(ScreenSize);
+        F.setVisible(true);
+        F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
